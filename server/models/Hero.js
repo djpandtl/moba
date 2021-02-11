617545fced538595d99d9a91e51dd0c996568fca
djpandtl <415237831@qq.com>
2021-02-11 23:24:15 +0800

    英雄模型的初步完成

--- server/models/Hero.js
+++ server/models/Hero.js
@@ -2,7 +2,37 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
   name: { type: String },
-  avatar: { type: String }
+  avatar: { type: String },
+  // parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Category'}
+  title: { type: String }, // 称号
+  categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
+
+  scores: {
+    difficult: { type: Number },
+    skills: { type: Number },
+    attack: { type: Number },
+    survive: { type: Number },
+  },
+  
+  skills: [{
+    icon: { type: String },
+    name: { type: String },
+    description: { type: String },
+    tips: { type: String }
+  }],
+  // 出装 1顺风 2逆风
+  items1: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }],
+  items2: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }],
+  
+
+  usageTips: { type: String },
+  battleTips: { type: String },
+  teamTips: { type: String },
+
+  partners: [{
+    hero: { type: mongoose.Schema.Types.ObjectId, ref: 'Hero'},
+    description: { type: String }
+  }] 
 })
 
 module.exports = mongoose.model('Hero', schema)
